82aae1ba19e079ff6e70558a31ba056cac506dc0
codr-mayank <44097941+codr-mayank@users.noreply.github.com>
2022-06-11 19:01:07 +0530

    add filtered names styling

--- app.vue
+++ app.vue
@@ -114,7 +114,14 @@
         Find Names 
       </button>
     </div>
-    {{ selectedNames }}
+    <div class="cards-container">
+      <div v-for="name in selectedNames" :key="name" class="card">
+        <h4>
+          {{ name }}
+        </h4>
+        <p>x</p>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -180,4 +187,30 @@
     margin-top: 1rem;
     cursor: pointer;
   }
+
+  .cards-container {
+    display: flex;
+    flex-wrap: wrap;
+    margin-top: 3rem;
+  }
+
+  .card {
+    background-color: rgb(27, 60, 138);
+    width: 28%;
+    color: #fff;
+    border-radius: 1rem;
+    padding: 0.1rem;
+    margin-right: 0.5rem;
+    margin-bottom: 1rem;
+    position: relative;
+  }
+
+  .card p {
+    position: absolute;
+    top: -20%;
+    left: 92.5%;
+    color: rgba(255, 255, 255, 0.388);
+    cursor: pointer;
+  }
+
 </style>
